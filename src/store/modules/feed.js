ef5652cc0f4eb23d6c43002e8e9aed1123494978
kolesnikovsenia <kolesnikovsena718@gmail.com>
2022-10-17 18:05:15 +0300

    fix:

--- src/store/modules/feed.js
+++ src/store/modules/feed.js
@@ -1,53 +1,54 @@
 import feedApi from '@/api/feed'
 
-const state ={
-	data: null,
-	isLoading: false,
-	error: null
+const state = {
+  data: null,
+  isLoading: false,
+  error: null,
 }
 
-export const mutationsTypes ={
-	getFeedStart: '[feed] Get feed start',
-	getFeedSuccess: '[feed] Get feed success',
-	getFeedFailure: '[feed] Get feed failure'
+export const mutationsTypes = {
+  getFeedStart: '[feed] Get feed start',
+  getFeedSuccess: '[feed] Get feed success',
+  getFeedFailure: '[feed] Get feed failure',
 }
 
 export const actionTypes = {
-	getFeed:'[feed] Get feed'
+  getFeed: '[feed] Get feed',
 }
 
 const mutations = {
-	[mutationsTypes.getFeedStart](state){
-		state.isLoading = true
-		state.data = null
-	},
-	[mutationsTypes.getFeedSuccess](state, payload){
-		state.isLoading = false
-		state.data = payload
-	},
-	[mutationsTypes.getFeedFailure](state){
-		state.isLoading = false
-	}
+  [mutationsTypes.getFeedStart](state) {
+    state.isLoading = true
+    state.data = null
+  },
+  [mutationsTypes.getFeedSuccess](state, payload) {
+    state.isLoading = false
+    state.data = payload
+  },
+  [mutationsTypes.getFeedFailure](state) {
+    state.isLoading = false
+  },
 }
 
 const actions = {
-	[actionTypes.getFeed](context, {apiUrl}){
-		return new Promise(resolve => {
-			context.commit(mutationsTypes.getFeedStart)
-			feedApi.getFeed(apiUrl)
-				.then(response => {
-					context.commit(mutationsTypes.getFeedSuccess, response.data)
-					resolve(response.data)
-				})
-				.catch(() => {
-					context.commit(mutationsTypes.getFeedFailure)
-				})
-		})
-	}
+  [actionTypes.getFeed](context, {apiUrl}) {
+    return new Promise((resolve) => {
+      context.commit(mutationsTypes.getFeedStart)
+      feedApi
+        .getFeed(apiUrl)
+        .then((response) => {
+          context.commit(mutationsTypes.getFeedSuccess, response.data)
+          resolve(response.data)
+        })
+        .catch(() => {
+          context.commit(mutationsTypes.getFeedFailure)
+        })
+    })
+  },
 }
 
 export default {
-	state,
-	actions,
-	mutations
+  state,
+  actions,
+  mutations,
 }
